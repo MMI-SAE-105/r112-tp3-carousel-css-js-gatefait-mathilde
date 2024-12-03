7a1a33f4f3d1ac21b08cdd56dc46af5ed513178e
gatefait-mathilde <mathilde.gatefait@edu.univ-fcomte.fr>
2024-12-03 22:57:03 +0100

    défilement de base précédent/suivant

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,18 @@
 // *** CAROUSEL ***
 // TODO
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+__
+if (carousel) {
+
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: -260, behavior: "smooth" });
+  });
+
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({ left: 260, behavior: "smooth" });
+  });
+}
